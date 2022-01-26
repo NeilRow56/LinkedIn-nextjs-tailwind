1742807fe2270c87419120f81bc29639b0783a42
Neil <neilrowland56@yahoo.com>
2022-01-26 05:00:28 +0000

    Completion of initial project setup with Tailwind

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 // pages/_app.js
 import React from 'react';
+import '../styles/globals.css';
 import { UserProvider } from '@auth0/nextjs-auth0';
 
 export default function App({ Component, pageProps }) {
