17237e172f3d26e13f0d1d553527d51951b36e7e
Neil <neilrowland56@yahoo.com>
2022-01-27 16:31:55 +0000

    Update for DarkMode and Header

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
-module.exports = {
+(module.exports = {
 	reactStrictMode: true,
-};
+}),
+	(module.exports = {
+		images: {
+			domains: ['rb.gy'],
+		},
+	});
--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,15 @@
 // pages/_app.js
 import React from 'react';
 import '../styles/globals.css';
+import { ThemeProvider } from 'next-themes';
 import { UserProvider } from '@auth0/nextjs-auth0';
 
 export default function App({ Component, pageProps }) {
 	return (
-		<UserProvider>
-			<Component {...pageProps} />
-		</UserProvider>
+		<ThemeProvider attribute="class">
+			<UserProvider>
+				<Component {...pageProps} />
+			</UserProvider>
+		</ThemeProvider>
 	);
 }
